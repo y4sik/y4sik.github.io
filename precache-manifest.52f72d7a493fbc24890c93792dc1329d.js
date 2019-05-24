51517b276d86a2281989b43683d68c2eeb66ea90
Android <hyliozqq@gmail.com>
2019-05-24 23:15:32 +0300

    Initial commit

---
+++ precache-manifest.52f72d7a493fbc24890c93792dc1329d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3cb558ef11650f2ce012de49bb0672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b539eefab4d3005777c",
+    "url": "/static/css/main.0b023888.chunk.css"
+  },
+  {
+    "revision": "219b9df3b03b48a2180f",
+    "url": "/static/js/2.3181acc2.chunk.js"
+  },
+  {
+    "revision": "0b539eefab4d3005777c",
+    "url": "/static/js/main.33fd0e5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
